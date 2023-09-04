61e3f233c648d99223890413c87ef83458944c4f
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-09-04 20:56:48 +0630

    Add: Layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,14 @@
-import './bootstrap';
-import '../css/app.css';
 import "primevue/resources/themes/md-light-indigo/theme.css";
+import '../css/app.css';
+import './bootstrap';
 
-import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
+import { createApp, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import Layout from "./Layouts/Layout.vue";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -24,4 +26,11 @@ createInertiaApp({
     progress: {
         color: '#4B5563',
     },
+ resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+    let page = pages[`./Pages/${name}.vue`]
+    page.default.layout = page.default.layout || Layout
+    return page
+    },
+
 });
