8205918cbcef08e37da0045509f34ece019f749a
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-09-04 20:56:28 +0630

    Add: primevue config

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,12 @@
 import './bootstrap';
 import '../css/app.css';
+import "primevue/resources/themes/md-light-indigo/theme.css";
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import PrimeVue from 'primevue/config';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -15,6 +17,8 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(PrimeVue)
+            .use(ToastService)
             .mount(el);
     },
     progress: {
