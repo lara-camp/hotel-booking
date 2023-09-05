c872cdfae458fbd987b88afa06535aeda3654ecd
Paing Min Soe <paingminsoe224@gmail.com>
2023-09-06 02:07:16 +0630

    added code for room controller

--- app/Http/Controllers/Admin/RoomController.php
+++ app/Http/Controllers/Admin/RoomController.php
@@ -3,17 +3,40 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Room;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class RoomController extends Controller
 {
     /**
+     * Create the controller instance.
+     */
+    public function __construct()
+    {
+        $this->authorizeResource(Room::class, 'rooms');
+    }
+
+    /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return Inertia::render('Room/Index');
+        return Inertia::render('Room/Index', [
+            'rooms' => Room::paginate(5)->through(fn($room) => [
+                'id' => $room->id,
+                'room_type' => $room->roomType->title,
+                'price' => $room->price,
+                'can' => [
+                    'update_room' => Auth::user()->can('update', $room),
+                ]
+            ]),
+            'can' =>[
+                'create_room' => Auth::user()->can('create', Room::class),
+            ]
+        ]);
     }
 
     /**
@@ -29,7 +52,16 @@ class RoomController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'room_number' => 'required|string|unique:rooms,room_number',
+            'room_type_id' => 'required|exists:room_types,id',
+            'price' => 'required|integer',
+            'available' => 'required|boolean',
+        ]);
+
+        Room::create($validated);
+
+        return redirect()->route('admin.room.index');
     }
 
     /**
@@ -37,7 +69,9 @@ class RoomController extends Controller
      */
     public function show(string $id)
     {
-        return Inertia::render('Room/Show');
+        return Inertia::render('Room/Show', [
+            'room' => Room::find($id)
+        ]);
     }
 
     /**
@@ -45,7 +79,9 @@ class RoomController extends Controller
      */
     public function edit(string $id)
     {
-        return Inertia::render('Room/Edit');
+        return Inertia::render('Room/Edit', [
+            'room' => Room::find($id)
+        ]);
     }
 
     /**
@@ -53,7 +89,14 @@ class RoomController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validated = $request->validate([
+            'room_number' => 'required|string|unique:rooms,room_number'.$id,
+            'room_type_id' => 'required|exists:room_types,id',
+            'price' => 'required|integer',
+            'available' => 'required|boolean',
+        ]);
+
+        return redirect()->route('admin.rooms.index');
     }
 
     /**
@@ -61,6 +104,9 @@ class RoomController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $room = Room::find($id);
+        $room->delete();
+
+        return redirect()->route('admin.rooms.index');
     }
 }
