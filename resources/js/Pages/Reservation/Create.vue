a972de9686aa18bf9fbc3efd39acc7a68a38369f
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-09-05 14:41:54 +0630

    Fix: Reservation Create according to updated db schema

--- resources/js/Pages/Reservation/Create.vue
+++ resources/js/Pages/Reservation/Create.vue
@@ -3,52 +3,37 @@
     <h1 class="text-3xl font-light">Create New Reservation</h1>
     <div class="[&>div]:mt-2">
       <div class="mb-3">
-        <h2 class=" mb-2 text-2xl">Guest</h2>
-        <div class="gap-x-3 flex">
-          <div class="flex flex-col">
-            <label for="name">Name</label>
-            <InputText id="name" v-model="reservationForm.name" />
-          </div>
-          <div class=" flex flex-col">
-            <label for="mobilPhone">Mobile Phone</label>
-            <InputText id="mobilPhone" v-model="reservationForm.mobile_phone" />
-          </div>
-          <div class="flex flex-col">
-            <label for="email">Email</label>
-            <InputText id="email" v-model="reservationForm.email" />
-          </div>
-        </div>
-      </div>
-      <div class="mb-3">
         <h2 class="mb-2 text-2xl">Room</h2>
         <div class="gap-x-3 flex mb-2">
-          <div class="flex flex-col">
+          <div class="w-52 flex flex-col">
             <label for="room">Room Number</label>
-            <Dropdown v-model="reservationForm.room_id" :options="availableRooms" optionLabel="room"
-              placeholder="Select a Room" editable />
+            <MultiSelect v-model="reservationForm.room_id" :options="availableRooms" filter optionLabel="room"
+              placeholder="Select rooms" :maxSelectedLabels="5" class="md:w-20rem w-full" />
           </div>
-        </div>
-        <h3 class=" text-xl">Room Configuration</h3>
-        <div class="gap-x-3 flex my-2">
           <div class="flex flex-col">
             <label for="totalPerson">Total Number Of Person</label>
             <InputNumber id="totoalPerson" v-model="reservationForm.total_person" />
           </div>
           <div class="flex flex-col">
-            <label for="extraBed">Extra Bed Count</label>
-            <InputNumber id="extraBed" v-model="reservationForm.extra_bed" />
+            <label for="totalPrice">Total Price</label>
+            <InputNumber id="totalPrice" v-model="reservationForm.total_price" mode="currency" currency="MMK" />
+          </div>
+        </div>
+        <div class="gap-x-3 flex mb-2">
+          <div class="flex flex-col">
+            <label for="status">Status</label>
+            <Textarea id="status" v-model="reservationForm.status" rows="5" cols="75" />
           </div>
         </div>
         <h3 class=" text-xl">Room Reservation Date</h3>
         <div class="gap-x-3 flex mt-2">
           <div class="flex flex-col">
             <label for="reservedFrom">From</label>
-            <Calendar v-model="reservationForm.reserved_day_from" :minDate="minDate" :manualInput="false"
-              id="reservedFrom" />
+            <Calendar v-model="reservationForm.from_date" :minDate="minDate" :manualInput="false" id="reservedFrom" />
           </div>
           <div class="flex flex-col">
             <label for="reservedTo">To</label>
-            <Calendar v-model="reservationForm.reserved_day_to" :minDate="minDate" :manualInput="false" id="reservedTo" />
+            <Calendar v-model="reservationForm.to_date" :minDate="minDate" :manualInput="false" id="reservedTo" />
           </div>
         </div>
       </div>
@@ -66,16 +51,21 @@
           </div>
         </div>
       </div>
+      <div class="mb-3">
+        <Button label="Add Reservation" outlined @click="submitForm" />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
   import { useForm } from '@inertiajs/vue3';
+  import Button from 'primevue/button';
   import Calendar from 'primevue/calendar';
-  import Dropdown from 'primevue/dropdown';
-  import InputText from 'primevue/inputtext';
   import InputNumber from 'primevue/inputnumber';
+  import MultiSelect from 'primevue/multiselect';
+  import Textarea from 'primevue/textarea';
+  import { useToast } from "primevue/usetoast";
 
   import { ref } from 'vue';
 
@@ -88,20 +78,24 @@
   ])
 
   const reservationForm = useForm({
-    name: "",
-    email: "",
-    mobile_phone: "",
-    extra_bed: 0,
-    room_id: availableRooms.value[0].room,
-    reserved_day_from: "",
-    reserved_day_to: "",
+    room_id: [],
+    total_person: 0,
+    total_price: 0,
+    status: "",
+    from_date: "",
+    to_date: "",
     checkin_time: "",
     checkout_time: "",
-    total_person: 0
   })
 
+  const toast = useToast();
+  function submitForm() {
+    reservationForm.post("/reservation", {
+      onSuccess: () => toast.add({ severity: 'success', summary: 'Success', detail: 'Added Reservation Successfully', life: 3000 }),
+    })
+  }
 
-  //For calculating min and max date
+  //For calculating min date
   const minDate = ref(new Date());
 
 </script>
