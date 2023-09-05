ebd49d5722d55b1fb53a6de62f693ad7409caaec
Paing Min Soe <paingminsoe224@gmail.com>
2023-09-06 01:58:01 +0630

    added migration files

--- database/migrations/2023_09_05_005046_create_rooms_table.php
+++ database/migrations/2023_09_05_005046_create_rooms_table.php
@@ -11,15 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('rooms', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedBigInteger('role_id')->default(2);
-            $table->rememberToken();
+            $table->string('room_number');
+            $table->unsignedBigInteger('room_type_id');
+            $table->integer('price');
+            $table->boolean('available')->default(true);
             $table->timestamps();
+            $table->foreign('room_type_id')->references('id')->on('room_types');
         });
     }
 
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('rooms');
     }
 };
--- database/migrations/2023_09_05_132027_create_reservations_table.php
+++ database/migrations/2023_09_05_132027_create_reservations_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reservations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedBigInteger('role_id')->default(2);
-            $table->rememberToken();
+            $table->unsignedBigInteger('user_id');
+            $table->integer('total_person');
+            $table->integer('total_price');
+            $table->date('from_date');
+            $table->date('to_date');
+            $table->dateTime('checkin_time');
+            $table->dateTime('checkout_time');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reservations');
     }
 };
