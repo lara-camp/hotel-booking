cb44bb52519678c5f70ab4951eb188fbf59497a2
Paing Min Soe <paingminsoe224@gmail.com>
2023-09-08 07:32:33 +0630

    Model, Controller and migration changed for profile image

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('profile_image')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->unsignedBigInteger('role_id')->default(2);
