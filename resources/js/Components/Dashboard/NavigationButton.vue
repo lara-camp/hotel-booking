24f1df566f70b24ba2b110e12a558d688ce04514
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-09-18 20:13:41 +0630

    Add: navigation button

---
+++ resources/js/Components/Dashboard/NavigationButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <Link :href="route(routeName)">
+  <div
+    class=" hover:text-indigo-600 hover:bg-white p-3 mb-2 text-white transition-colors duration-300 rounded cursor-pointer">
+    <div class="flex items-baseline justify-start">
+      <div class="w-1/6 ml-6 mr-4">
+        <span :class="[icon]"></span>
+      </div>
+      <p class=" text-xl font-semibold">
+        <slot />
+      </p>
+    </div>
+  </div>
+  </Link>
+</template>
+<script setup>
+  import { Link } from "@inertiajs/vue3";
+  import { defineProps } from "vue";
+  const props = defineProps({
+    icon: String,
+    routeName: String
+  })
+</script>
