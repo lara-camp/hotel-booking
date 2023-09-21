73713e24f08ed9d3ad5fb9dad8c76749191e1d21
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-09-21 20:28:03 +0630

    Add: Error Layout

---
+++ resources/js/Layouts/ErrorLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex justify-center min-w-full min-h-screen bg-indigo-600">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped></style>
