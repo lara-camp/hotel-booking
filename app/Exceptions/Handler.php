3abd8e1bed3551e2e9b7156fe5f48653964b415f
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-09-21 20:28:57 +0630

    Add: Error page to Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,10 +3,10 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Inertia\Inertia;
 use Throwable;
 
-class Handler extends ExceptionHandler
-{
+class Handler extends ExceptionHandler {
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -21,10 +21,16 @@ class Handler extends ExceptionHandler
     /**
      * Register the exception handling callbacks for the application.
      */
-    public function register(): void
-    {
+    public function register(): void {
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e) {
+        $response = parent::render($request, $e);
+        return Inertia::render('Error', [
+            'status' => $response->status()
+        ]);
     }
 }
