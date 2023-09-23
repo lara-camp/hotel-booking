720a9e3adc0260af0a6bbb301b6f1007caa6d38a
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-09-23 19:30:39 +0630

    Fix: create and edit dialog issue in roomtype

--- resources/js/Components/RoomType/CreateDialog.vue
+++ resources/js/Components/RoomType/CreateDialog.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="w-[30rem]">
-    <form @submit.prevent="() => roomTypeForm.post(route('admin.room-types.store'))">
+    <form @submit.prevent="submitForm">
       <h1 class="mb-2 text-3xl font-bold">Create Room Type</h1>
       <div class="gap-x-2 flex mb-2">
         <div class=" flex flex-col w-full">
           <label for="checkin">Room Type</label>
           <InputText type="text" v-model="roomTypeForm.name" autofocus />
+          <InlineMessage v-if="roomTypeForm.errors.name" severity="error" class="mt-2">
+            {{ roomTypeForm.errors.name }}
+          </InlineMessage>
         </div>
       </div>
       <div class="flex justify-end">
         <Button label="Create" outlined type="submit" :loading="roomTypeForm.processing" />
       </div>
     </form>
-
   </div>
   <Toast position="bottom-right" />
 </template>
@@ -24,6 +26,10 @@
   import Toast from "primevue/toast";
   import { useToast } from "primevue/usetoast";
   import { inject } from 'vue';
+
+  defineProps({
+    errors: Object
+  })
 
   const toast = useToast();
 
