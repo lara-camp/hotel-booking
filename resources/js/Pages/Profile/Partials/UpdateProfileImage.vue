e8d4dad3fb3779e4b7d2f5601ebbf0883a5a8b1d
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-09-26 20:07:02 +0630

    Fix: Profile image not appearing in profile.edit

--- resources/js/Pages/Profile/Partials/UpdateProfileImage.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileImage.vue
@@ -2,21 +2,24 @@
   <form class="relative md:w-1/2 h-auto max-h-[18rem] bg-slate-200/50 rounded">
     <input type="file" accept="image/jpeg,image/png" class="hidden" id="profilePicture" ref="profilePictureInputRef"
       @input="handleProfileInput">
-    <img :src="previewProfilePhoto || user.profile_img" :alt="`Profile image of ${user.name}`"
-      v-if="previewProfilePhoto || user.profile_img" class="object-cover w-full h-full rounded">
+    <img :src="previewProfilePhoto" :alt="`Profile image of ${user.name}`" class="object-cover w-full h-full rounded"
+      v-if="previewProfilePhoto">
+    <img :src="user.profile_image_path" :alt="`Profile image of ${user.name}`" class="object-cover w-full h-full rounded"
+      v-else-if="user.profile_image_path">
     <div class="flex flex-col justify-center w-full h-full text-center" v-else>
       <p class="">There is no profile image.</p>
     </div>
     <Button icon="pi pi-pencil" class="!absolute bottom-0 left-0 bg-indigo-700 hover:bg-indigo-700"
       @click="() => profilePictureInputRef.click()" />
-    <Button icon="pi pi-save" class="!absolute bottom-0 left-16 bg-indigo-700 hover:bg-indigo-700" :loading="form.processing" @click="submit"/>
+    <Button icon="pi pi-save" class="!absolute bottom-0 left-16 bg-indigo-700 hover:bg-indigo-700"
+      :loading="form.processing" @click="submit" />
   </form>
 </template>
 
 <script setup>
   import { useForm, usePage } from "@inertiajs/vue3";
-import Button from 'primevue/button';
-import { ref } from "vue";
+  import Button from 'primevue/button';
+  import { ref } from "vue";
 
   const user = usePage().props.auth.user;
 
