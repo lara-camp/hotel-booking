babb5ec5469147497cd2edfe98e01e1860cc9e18
KaungZayY <kaungzayyan19@gmail.com>
2023-09-30 08:51:06 +0630

    redirect user based on role after login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -34,7 +34,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        return redirect(auth()->user()->getRedirectRoute());
     }
 
     /**
