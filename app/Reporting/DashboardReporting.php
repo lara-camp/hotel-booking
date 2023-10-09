972749d241ca1fa4378bd6e04dfa47f99c80ee2c
Thet Htoo Aung <ththtooaung.e@gmail.com>
2023-10-09 11:38:39 +0630

    features update today-avaiable-rooms, today-reserved-rooms, monthly-guests, monthly-amount

--- app/Reporting/DashboardReporting.php
+++ app/Reporting/DashboardReporting.php
@@ -7,12 +7,8 @@ use App\Models\Room;
 
 class DashboardReporting {
 
-    public function popularRoomTypes() : array {
-        //defining the date to get reservations
-        date_default_timezone_set("Asia/Yangon");
-        $month = date('M');
-        $start_date = date('Y-m-d h:i:s', strtotime("$month 1"));
-        $end_date = date('Y-m-d h:i:s', strtotime('+1 month', strtotime($month)));
+    public function popularRoomTypes($start_date, $end_date) : array {
+        
         $reservations = Reservation::whereBetween('from_date',[$start_date,$end_date])
                         ->get();
         //calculating the times of booking a specific room type
@@ -28,28 +24,40 @@ class DashboardReporting {
         return $room_types;
     }
 
-    public function availableRooms($from_date = 0, $to_date = 0) : array { //[roomtype => numberOfRooms]
+    public function availableRooms($from_date = 0, $to_date = 0) : object {
         //get reservations of today
         $today = date('Y-m-d', strtotime('today'));
         $from_date = $from_date ? date('Y-m-d', strtotime($from_date)) : $today;
         $to_date = $to_date ? date('Y-m-d', strtotime($to_date)) : $today;
 
-        $availableRooms = Room::whereDoesntHave('reservations',function($query) use ($from_date, $to_date) {
+        $availableRooms = Room::whereDoesntHave('reservations', function($query) use ($from_date, $to_date) {
             $query->whereBetween('from_date',[$from_date, $to_date])
             ->orWhereBetween('to_date', [$from_date, $to_date])
+            ->orWhere('from_date', '=', $from_date)
             ->orWhere('from_date', '=', $to_date)
             ->orWhere('to_date', '=', $from_date)
-            //check if the from_date is between the reservation date
-            ->orWhere(function ($query) use ($from_date, $to_date) {
-                $query->where('from_date', '>=', $from_date)
-                        ->where('to_date', '<=', $to_date);
-            })
-            //check if the to_date is between the reservation date
+            ->orWhere('to_date', '=', $to_date)
+            //check if the reservation date is between from_date and to
             ->orWhere(function ($query) use ($from_date, $to_date) {
                 $query->where('from_date', '<=', $from_date)
                     ->where('to_date', '>=', $to_date);
             });
+        })->get(); 
+        return $availableRooms;
+    }
+
+    //reserved rooms for today
+    public function reservedRooms() : object {
+        $today = date('Y-m-d', strtotime('today'));
+        $reservedRooms = Room::whereHas('reservations', function($query) use ($today) {
+            $query->where('from_date', '=', $today);
         })->get();
+        return $reservedRooms;
+    }
+
+    public function availableRoomTypes($from_date = 0, $to_date = 0) : array { //[roomtype => numberOfRooms]
+        //getting availble rooms first
+        $availableRooms = $this->availableRooms($from_date, $to_date);
 
         //calculate the numbers of available rooms depending on a particular room type
         $availableRoomTypes = [];
@@ -57,7 +65,7 @@ class DashboardReporting {
             if(!isset($availableRoomTypes[$availableRoom->roomType->name])) $availableRoomTypes[$availableRoom->roomType->name] = 1;
             else $availableRoomTypes[$availableRoom->roomType->name] ++;
         }
-
         return $availableRoomTypes;
     }
+
 }
