281af9682f1f7906d44cd6d2dc5d4cf5db4457f2
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-10-10 22:33:57 +0630

    Add: ui for user reservation

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -9,23 +9,29 @@
     canRegister: {
       type: Boolean,
     },
+    errors: Object,
+    popularRoomTypes: Array,
+    searchRooms: {
+      required: false
+    }
   });
 </script>
 <template>
   <Head title="Welcome" />
-  <div class="h-[1000rem]  bg-slate-100 rounded p-3">
+  <div class="bg-slate-100 p-3 rounded">
     <div class="md:max-w-[75rem] max-w-2xl flex-col items-center justify-center mx-auto p-3">
-      <div class="my-16">
+      <div class="my-24">
         <h1 class=" mb-3 text-6xl font-bold">Welcome to Laracamp Hotel</h1>
         <h2 class="text-4xl font-normal">Reserve your room</h2>
       </div>
       <WelcomeFiler />
-      <div class="h-96 w-full"></div>
+      <SearchRooms :search-rooms="searchRooms" :errors="errors" v-if="searchRooms"/>
     </div>
   </div>
 </template>
 <script>
   import IndexLayout from '@/Layouts/IndexLayout.vue';
+  import SearchRooms from "@/Components/Welcome/SearchRooms.vue";
 
   export default {
     layout: IndexLayout
