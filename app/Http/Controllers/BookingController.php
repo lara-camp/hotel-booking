3a984ea451e17406c9669822ff834a01db4e741b
Thet Htoo Aung <ththtooaung.e@gmail.com>
2023-10-13 14:19:25 +0630

    number of admin-reserved-rooms and user-reserved-rooms

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreBookingRequest;
+use App\Http\Requests\ReservationRequest;
+use App\Mail\BookingNotificationMail;
 use App\Models\Reservation;
 use App\Models\Room;
 use App\Reporting\DashboardReporting;
 use Carbon\Carbon;
+use Illuminate\Validation\ValidationException;
 use Exception;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Inertia\Inertia;
 
 class BookingController extends Controller
@@ -59,17 +62,35 @@ class BookingController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreBookingRequest $request)
-    {
-        dd($request->all());
+    public function store(ReservationRequest $request)
+    {   
+        $from_date = date('Y-m-d', strtotime($request->from_date));
+        $to_date = date('Y-m-d', strtotime($request->to_date));
+
+        $report = new DashboardReporting();
+        $availableRooms = $report->availableRooms($from_date, $to_date);
+        
+        $total_price = 0;
+
+        foreach($request->room_id as $room) {
+            if($availableRooms->find($room)) {
+                $total_price += $availableRooms->find($room)->price;
+            } else {
+                throw ValidationException::withMessages(['room_id' => "Some of the rooms are reserved on given date."]);
+            }
+        }
+
+
         DB::beginTransaction();
 
         try {
             $reservation = new Reservation();
             $reservation->user_id = Auth::user()->id;
+            $reservation->guest_name = $request->guest_name;
+            $reservation->total_person = $request->total_person;
+            $reservation->total_price = $total_price;
             $reservation->from_date =date('Y-m-d',strtotime($request->from_date));
             $reservation->to_date = date('Y-m-d',strtotime($request->to_date));
-            $reservation->status = $request->status;
             $reservation->checkin_time = $request->checkin_time;
             $reservation->checkout_time = $request->checkout_time;
             $reservation->save();
@@ -77,6 +98,8 @@ class BookingController extends Controller
             $reservation->rooms()->attach($request->room_id);
 
             DB::commit();
+            Mail::to(Auth::user()->email)->send(new BookingNotificationMail($reservation));
+            return redirect()->route('admin.reservations.index');
 
         } catch (Exception $e) {
             DB::rollBack();
