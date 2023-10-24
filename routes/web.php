71b3d4945e23480c334c4e68c3b99507444573d5
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-10-24 19:28:08 +0630

    Update: route for archiving

--- routes/web.php
+++ routes/web.php
@@ -22,16 +22,16 @@ use Illuminate\Support\Facades\Route;
 Route::get("/", [BookingController::class, "index"])->name("index");
 
 Route::middleware(["auth"])->as("user.")->group(function () {
-    Route::get("/my-reservations",[BookingController::class,"reservations"])->name("reservations");
+    Route::get("/my-reservations", [BookingController::class, "reservations"])->name("reservations");
     Route::inertia("/profile", "User/Profile")->name("profile");
     Route::patch("/profile", [ProfileController::class, "update"])->name("profile.update");
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::post("/profile/image", [ProfileController::class, 'updateProfileImage'])->name("profile.updateProfileImage");
 
-    Route::post("/reserve",[BookingController::class,"store"])->name("reserve");
+    Route::post("/reserve", [BookingController::class, "store"])->name("reserve");
 });
 
-Route::middleware('auth')->resource('/',BookingController::class)->only(['create', 'store']);
+Route::middleware('auth')->resource('/', BookingController::class)->only(['create', 'store']);
 
 Route::middleware(['auth', 'admin'])->prefix("admin")->as("admin.")->group(function () {
     Route::get("/", ReportingController::class)->name("index");
@@ -41,23 +41,24 @@ Route::middleware(['auth', 'admin'])->prefix("admin")->as("admin.")->group(funct
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::post("/profile/image", [ProfileController::class, 'updateProfileImage'])->name("profile.updateProfileImage");
-    
-    //reservations soft delete
-    Route::get("/reservations/archives", [ReservationController::class, 'archive']);
-    Route::patch("/reservations/{reservation}/restore", [RoomTypeController::class, "restore"])->name("reservations.restore");
-    Route::delete("/reservations/{reservation}/force-delete", [RoomTypeController::class, "forceDelete"])->name("reservations.force-delete");
 
-    // Resources
+    // Reservation
     Route::resource('reservations', ReservationController::class);
+    Route::get("/reservations/archives", [ReservationController::class, 'archive'])->name("reservations.archive");
+    Route::patch("/reservations/{reservation}/restore", [ReservationController::class, "restore"])->name("reservations.restore");
+    Route::delete("/reservations/{reservation}/force-delete", [ReservationController::class, "forceDelete"])->name("reservations.force-delete");
+
+    // Room
     Route::resource('rooms', RoomController::class);
+    Route::get("/rooms/archives", [RoomController::class, "archive"])->name("rooms.archive");
+    Route::patch("/rooms/{rooms}/restore", [RoomController::class, "restore"])->name("rooms.restore");
+    Route::delete("/rooms/{rooms}/force-delete", [RoomController::class, "forceDelete"])->name("rooms.force-delete");
+
+    // Room Type
     Route::resource('room-types', RoomTypeController::class)->except(['create', 'edit', 'show']);
     Route::get('/room-types/archives', [RoomTypeController::class, 'archives'])->name('room-types.archives');
-    Route::get("/room-types/test", [RoomTypeController::class, "test"])->name("room-types.test");
     Route::patch("/room-types/{room_types}/restore", [RoomTypeController::class, "restore"])->name("room-types.restore");
     Route::delete("/room-types/{room_types}/force-delete", [RoomTypeController::class, "forceDelete"])->name("room-types.force-delete");
-
-
-
 });
 
 require __DIR__ . '/auth.php';
